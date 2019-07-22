fe7527c303d579e6220e091b25d8a1139000a534
Paul Goux <paul.goux@wanadoo.fr>
2019-07-22 21:04:33 +0200

    ça fonctionne pas

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 const Discord = require('discord.js');
+const dateFormat = require('dateformat');
 const client = new Discord.Client();
+
+const nbMessageSave = 100;
+var tabMessages = {};
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if(!(msg.author in tabMessages))
+        tabMessages[msg.author] = [];
+    if(msg.content !== '!fbi')
+        tabMessages[msg.author].push(msg);
+    if(tabMessages[msg.author].length > nbMessageSave)
+        tabMessages[msg.author].shift();
+    if(msg.content === '!fbi'){
+        let messages = "";
+        let personne = msg.author;
+        if(msg.isMemberMentioned())
+            msg.mentions.members.first().id
+        tabMessages[personne].forEach(function(item, index, array){
+            messages += "[" + dateFormat(msg.createdAt, "HH:MM:ss") + "] `" + item.content + "`\n";
+        });
+        msg.channel.send(messages);
     }
 });
 
